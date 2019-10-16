2ea825af70d633f01e6e232667ea44626424db26
tgd1973 <tgd1973@yahoo.com>
2019-10-17 01:17:58 +0200

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -33,7 +33,7 @@ GATEWAY=$(ip route | grep default | awk '{print $3}')
 DNS1="9.9.9.9"
 DNS2="149.112.112.112"
 # Repo
-GITHUB_REPO="https://raw.githubusercontent.com/nextcloud/vm/master"
+GITHUB_REPO="https://raw.githubusercontent.com/tgd1973/Nexcloud-vm/master"
 STATIC="$GITHUB_REPO/static"
 LETS_ENC="$GITHUB_REPO/lets-encrypt"
 APP="$GITHUB_REPO/apps"
